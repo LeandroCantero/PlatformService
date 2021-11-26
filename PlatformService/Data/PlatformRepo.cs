40470da776cb070531ed1c227de121dca04b0eeb
Leandro Cantero <lcantero@huenei.com>
2021-11-26 09:18:37 -0300

    push

---
+++ PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepo : IPlatformRepo
+    {
+        public void CreatePlatform(Platform plat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Platform GetPlatformById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
